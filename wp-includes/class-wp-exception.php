bf78e63100323ba8a00048c0ae659104d79c17ed
CodesCamp <how2code546@gmail.com>
2025-02-02 00:53:54 +0000

    Add files via upload

---
+++ wp-includes/class-wp-exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WP_Exception class
+ *
+ * @package CodesCamp
+ * @since 6.7.0
+ */
+
+/**
+ * Core base Exception class.
+ *
+ * Future, more specific, Exceptions should always extend this base class.
+ *
+ * @since 6.7.0
+ */
+class WP_Exception extends Exception {}
